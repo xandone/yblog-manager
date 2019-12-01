82382fc47c1b4da6a8631496b8e5b712cc7c6b74
xandone <765478955@qq.com>
2019-12-01 12:47:44 +0800

    init

--- src/pages/artAdd.vue
+++ src/pages/artAdd.vue
@@ -111,8 +111,8 @@ export default {
                 // 'emoticon',  // 表情
                 'image', // 插入图片
                 'table', // 表格
-                'video', // 插入视频
-                // 'code',  // 插入代码
+                // 'video', // 插入视频
+                'code',  // 插入代码
                 'undo', // 撤销
                 'redo' // 重复
             ]
